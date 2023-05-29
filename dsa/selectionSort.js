1bd642a41d58f5d02e379899b76c3066349f20d9
rudra pramanik <rudrahimangshoo2020@gmail.com>
2023-05-30 03:50:09 +0600

    selection-sort

---
+++ dsa/selectionSort.js
@@ -0,0 +1,16 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[lowest];
+    arr[lowest] = temp;
+  }
+  return arr;
+}
+
+console.log(selectionSort([4, 6, 11, -8, 9, 11]));
